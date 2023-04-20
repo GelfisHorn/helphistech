0601cbe0a6949e77326d7b57e818b79d275a2b5f
GelfisHorn <helphis.tech@gmail.com>
2023-04-20 10:01:39 -0300

    Replaced login icons from FontAwesome to HeroIcons

--- client/src/components/en/login/LoginForm.jsx
+++ client/src/components/en/login/LoginForm.jsx
@@ -67,14 +67,20 @@ export default function LoginForm() {
                         reference={email}
                         placeholder={"Email"}
                         id={"email"}
-                        icon={"fa-light fa-envelope"}
+                        icon={<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1} stroke="currentColor" className="w-6 h-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" />
+                        </svg>
+                        }
                         inputType={"email"}
                     />
                     <Input 
                         reference={password}
                         placeholder={"Password"}
                         id={"pass"}
-                        icon={"fa-light fa-lock"}
+                        icon={<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1} stroke="currentColor" className="w-6 h-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z" />
+                        </svg>
+                        }
                         inputType={"password"}
                     />
                 </div>
@@ -100,8 +106,8 @@ function Input({ reference, placeholder, id, icon, inputType }) {
 
     return (
         <div className={`${darkMode ? 'bg-neutral-800' : 'bg-white'} flex items-center h-14 rounded-md`}>
-            <div className="grid place-content-center w-[3.5rem] h-full">
-                <i className={`${icon} text-xl text-zinc-500`}></i>
+            <div className="grid place-content-center w-[3.5rem] h-full text-neutral-600">
+                {icon}
             </div>
             <div className="w-full">
                 <input ref={reference} className={`${darkMode ? 'text-zinc-200' : 'text-black'} w-full pr-3 outline-none font-light bg-transparent`} type={inputType} name="" id={id} placeholder={placeholder} />
