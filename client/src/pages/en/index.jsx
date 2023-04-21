c1d0c4a65fa58bbf09b3be03e2493c05d95c2fe5
GelfisHorn <helphis.tech@gmail.com>
2023-04-20 23:56:04 -0300

    Client: home page responsive.

--- client/src/pages/en/index.jsx
+++ client/src/pages/en/index.jsx
@@ -24,11 +24,11 @@ export default function Home() {
 				<div className={'relative overflow-hidden'} id="hero">
 					<Image className="absolute top-0 w-full h-full object-cover" src={`${darkMode ? '/home/hero/wave/dark/wave.webp' : '/home/hero/wave/light/wave.webp'}`} fill={true} alt="" priority={true} />
 					<Navbar />
-					<section className="relative flex items-center justify-center xl:px-20 2xl:px-0 min-h-[80rem] xl:min-h-[50rem]" style={{height: 'calc(100vh - 5rem)', zIndex: '1'}}>
-						<div className="max-w-7xl flex flex-col xl:flex-row items-center gap-12 w-full">
-							<div className="flex flex-col gap-10 text-center xl:text-left xl:max-w-[33rem] 2xl:max-w-[37rem] px-20 xl:px-0">
+					<section className="relative flex items-center justify-center px-6 sm:px-10 lg:px-20 2xl:px-0 min-h-[57rem] sm:min-h-[55rem] md:min-h-[65rem] lg:min-h-[75rem] xl:min-h-[50rem]" style={{height: 'calc(100vh - 5rem)', zIndex: '1'}}>
+						<div className="max-w-7xl 2xl:max-w-[90rem] flex flex-col xl:flex-row items-center gap-12 2xl:gap-20 w-full">
+							<div className="flex flex-col gap-10 text-center xl:text-left xl:max-w-[33rem] 2xl:max-w-[39rem]">
 								<div className={`flex flex-col gap-5`}>
-									<h1 className={`${darkMode ? 'title-dark' : 'title-light'} text-6xl 2xl:text-7xl font-bold leading-[5rem] 2xl:leading-[5.5rem]`}>Custom web development for your business</h1>
+									<h1 className={`${darkMode ? 'title-dark' : 'title-light'} text-4xl leading-[4rem] md:text-5xl md:leading-[4.5rem] lg:text-6xl lg:leading-[5rem] 2xl:text-7xl 2xl:leading-[5.5rem] font-bold`}>Custom web development for your business</h1>
 									<p className={`${darkMode ? 'description-dark font-light' : 'description-light'} 2xl:text-lg`}>We offer customized solutions to help your business stand out online. From website design and development to app programming and search engine optimization, our team of experts can help you achieve your online goals.</p>
 								</div>
 								<div className="flex justify-center xl:justify-start">
@@ -45,24 +45,24 @@ export default function Home() {
 						</div>
 					</section>
 				</div>
-				<section className={`px-20 2xl:px-0 ${darkMode ? 'section-bg-dark' : 'section-bg-light'} py-28 overflow-hidden`} id="our-services">
-					<div className="max-w-7xl mx-auto z-10 relative">
+				<section className={`px-6 sm:px-10 lg:px-20 2xl:px-0 ${darkMode ? 'section-bg-dark' : 'section-bg-light'} py-28 overflow-hidden`} id="our-services">
+					<div className="max-w-7xl 2xl:max-w-[90rem] mx-auto z-10 relative">
 						<div className="flex items-center gap-20 justify-between w-full">
 							<div className="blur-shadow -left-28 -top-28"></div>
 							<div className="flex flex-col gap-20">
-								<div className="flex flex-col gap-5 relative">
+								<div className="flex flex-col items-center sm:items-start gap-5 relative">
 									<div className="flex flex-col">
 										<span className={`uppercase font-semibold ${darkMode ? 'subtitle-dark' : 'subtitle-light'}`}>Our services</span>
 									</div>
-									<div className="flex items-start gap-20">
+									<div className="flex flex-col xl:flex-row justify-center sm:items-start gap-10 xl:gap-20">
 										<div>
-											<h2 className={`flex flex-col gap-5 text-5xl font-bold whitespace-nowrap ${darkMode ? 'title-dark' : 'title-light'}`}>
+											<h2 className={`flex flex-col items-center sm:items-start gap-5 text-3xl sm:text-5xl font-bold whitespace-nowrap ${darkMode ? 'title-dark' : 'title-light'}`}>
 												<div>What <span className="text-primary">Services</span></div>
 												<div>{"We're Offering"}</div>
 											</h2>
 										</div>
 										<div className={`flex flex-col gap-5 ${darkMode ? 'description-dark font-light' : 'description-light'}`}>
-											<p>At HelphisTech, we offer a wide range of web development services to help businesses create a strong online presence and achieve their digital goals. Our team of experienced developers is skilled in creating custom websites, web applications, and e-commerce platforms that are tailored to meet the unique needs of each client.</p>
+											<p className="text-center sm:text-left">At HelphisTech, we offer a wide range of web development services to help businesses create a strong online presence and achieve their digital goals. Our team of experienced developers is skilled in creating custom websites, web applications, and e-commerce platforms that are tailored to meet the unique needs of each client.</p>
 											{/* We use the latest technologies and industry best practices to ensure that our projects are of the highest quality, and we work closely with our clients to ensure that they are satisfied with the final product. */}
 										</div>
 									</div>
@@ -97,51 +97,53 @@ export default function Home() {
 						</div>
 					</div>
 				</section>
-				<section className={`relative overflow-hidden ${darkMode ? 'section-bg-dark border-[#19191F]' : 'section-bg-light border-zinc-300'} flex items-center py-28 px-20 2xl:px-0 border-t`} id="our-technologies">
-					<div className="blur-shadow -top-6 -left-6"></div>
-					<div className="flex flex-col gap-20 overflow-hidden max-w-7xl mx-auto">
-						<div className="flex flex-col gap-4 text-white relative">
-							<div className="flex flex-col gap-3">
-								<span className={`uppercase font-semibold ${darkMode ? 'subtitle-dark' : 'subtitle-light'}`}>Our technologies</span>
-								<h2 className={`flex flex-col gap-5 text-5xl font-bold whitespace-nowrap ${darkMode ? 'title-dark' : 'title-light'}`}>
-									<div>These are the <span className="text-primary">Technologies</span></div> 
-									<div>we use the most</div>
-								</h2>
+				<section className={`relative overflow-hidden ${darkMode ? 'section-bg-dark border-[#19191F]' : 'section-bg-light border-zinc-300'} flex items-center py-28 px-6 sm:px-10 lg:px-20 2xl:px-0 border-t`} id="our-technologies">
+					<div className="max-w-7xl 2xl:max-w-[90rem] mx-auto w-full relative">
+						<div className="blur-shadow -top-28 -left-28"></div>
+						<div className="max-w-7xl 2xl:max-w-[90rem] mx-auto flex flex-col gap-20 overflow-hidden">
+							<div className="flex flex-col gap-4 text-white relative">
+								<div className="flex flex-col items-center sm:items-start gap-5">
+									<span className={`uppercase font-semibold ${darkMode ? 'subtitle-dark' : 'subtitle-light'}`}>Our technologies</span>
+									<h2 className={`flex flex-col items-center sm:items-start gap-5 text-3xl sm:text-5xl font-bold ${darkMode ? 'title-dark' : 'title-light'}`}>
+										<div className="flex flex-col md:flex-row gap-5 md:gap-2">These are the <span className="text-primary">Technologies</span></div> 
+										<div>we use the most</div>
+									</h2>
+								</div>
+								<div className={`2xl:text-lg ${darkMode ? 'description-dark font-light' : 'description-light'}`}>
+									<p className="text-center sm:text-left">The appropriate technology enriches the project. Here are some of the ones we use the most:</p>
+								</div>
 							</div>
-							<div className={`2xl:text-lg ${darkMode ? 'description-dark font-light' : 'description-light'}`}>
-								<p>The appropriate technology enriches the project. Here are some of the ones we use the most:</p>
+							<div className="overflow-x-scroll hide-scroll">
+								<div className="grid grid-cols-5 gap-y-10 gap-x-14 xl:gap-x-28 select-none min-w-[60rem]">
+									<TechnologyImage src={"/technologies/react.webp"} alt={"React technology image"} />
+									<TechnologyImage src={"/technologies/nextjs.webp"} alt={"Next js technology image"} darkmode={"/technologies/darkmode/nextjs.webp"} />
+									<TechnologyImage src={"/technologies/angular.webp"} alt={"Angular technology image"} />
+									<TechnologyImage src={"/technologies/vuejs.webp"} alt={"Vue technology image"} />
+									<TechnologyImage src={"/technologies/astro.webp"} alt={"Astro technology image"} darkmode={"/technologies/darkmode/astro.webp"} />
+									<TechnologyImage src={"/technologies/tailwind.webp"} alt={"Tailwind technology image"} />
+									<TechnologyImage src={"/technologies/nodejs.webp"} alt={"Node js technology image"} darkmode={"/technologies/darkmode/nodejs.webp"} />
+									<TechnologyImage src={"/technologies/mongodb.webp"} alt={"MongoDB technology image"} />
+									<TechnologyImage src={"/technologies/mysql.webp"} alt={"MySQL technology image"} />
+									<TechnologyImage src={"/technologies/socketio.webp"} alt={"Socket.io technology image"} darkmode={"/technologies/darkmode/socketio.webp"} />
+								</div>
 							</div>
+							<Link className="flex justify-center" href={"#"}>
+								<div className="flex items-center gap-2 text-primary hover:text-primary-2 hover:underline transition-colors">
+									<div>See more</div>
+									<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+										<path strokeLinecap="round" strokeLinejoin="round" d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3" />
+									</svg>
+								</div>
+							</Link>
 						</div>
-						<div className="overflow-x-scroll hide-scroll">
-							<div className="grid grid-cols-5 gap-y-10 gap-x-14 xl:gap-x-28 select-none min-w-[60rem]">
-								<TechnologyImage src={"/technologies/react.webp"} alt={"React technology image"} />
-								<TechnologyImage src={"/technologies/nextjs.webp"} alt={"Next js technology image"} darkmode={"/technologies/darkmode/nextjs.webp"} />
-								<TechnologyImage src={"/technologies/angular.webp"} alt={"Angular technology image"} />
-								<TechnologyImage src={"/technologies/vuejs.webp"} alt={"Vue technology image"} />
-								<TechnologyImage src={"/technologies/astro.webp"} alt={"Astro technology image"} darkmode={"/technologies/darkmode/astro.webp"} />
-								<TechnologyImage src={"/technologies/tailwind.webp"} alt={"Tailwind technology image"} />
-								<TechnologyImage src={"/technologies/nodejs.webp"} alt={"Node js technology image"} darkmode={"/technologies/darkmode/nodejs.webp"} />
-								<TechnologyImage src={"/technologies/mongodb.webp"} alt={"MongoDB technology image"} />
-								<TechnologyImage src={"/technologies/mysql.webp"} alt={"MySQL technology image"} />
-								<TechnologyImage src={"/technologies/socketio.webp"} alt={"Socket.io technology image"} darkmode={"/technologies/darkmode/socketio.webp"} />
-							</div>
-						</div>
-						<Link className="flex justify-center" href={"#"}>
-							<div className="flex items-center gap-2 text-primary hover:text-primary-2 hover:underline transition-colors">
-								<div>See more</div>
-								<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-									<path strokeLinecap="round" strokeLinejoin="round" d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3" />
-								</svg>
-							</div>
-						</Link>
 					</div>
 				</section>
-				<section className="flex flex-col gap-16 py-28 text-center" id="index_section_startmyproject">
+				<section className="px-6 sm:px-10 lg:px-20 2xl:px-0 flex flex-col gap-16 py-28 text-center" id="index_section_startmyproject">
 					<div className="flex flex-col gap-8">
 						<div className={`${darkMode ? 'subtitle-dark' : 'subtitle-light'} text-lg uppercase font-semibold`}>
 							<span>Do you have an idea?</span>
 						</div>
-						<div className="w-2/3 2xl:w-2/4 mx-auto">
+						<div className="lg:w-3/4 2xl:w-2/4 mx-auto">
 							<span className={`${darkMode ? 'title-dark' : 'title-light'} text-[2.25rem] sm:text-[3rem] md:text-[3.75rem] lg:text-[4.5rem] font-semibold`}>We only need an idea or a problem for us to carry out your project</span>
 						</div>
 					</div>
@@ -212,9 +214,9 @@ function ServicesOption({ title, description, href }) {
 
 	return (
 		<div onClick={handleShowDescription} className={`flex flex-col gap-5 py-9 cursor-pointer select-none ${darkMode ? 'subtitle-dark border-[#19191F] hover:text-zinc-200' : 'text-neutral-600 hover:text-black border-neutral-300'} transition-colors`}>
-			<div className={`flex items-center justify-between ${showDescription ? darkMode ? 'text-zinc-200' : 'text-black' : null}`}>
-				<h3 className="font-extralight text-4xl">{title}</h3>
-				<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={.7} stroke="currentColor" className={`w-12 h-12 ${showDescription ? 'rotate-180' : 'rotate-0'} transition-all`}>
+			<div className={`flex items-center justify-between gap-5 sm:gap-0 ${showDescription ? darkMode ? 'text-zinc-200' : 'text-black' : null}`}>
+				<h3 className="font-extralight text-xl sm:text-2xl lg:text-3xl xl:text-4xl">{title}</h3>
+				<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={.7} stroke="currentColor" className={`w-8 h-8 xl:w-12 xl:h-12 ${showDescription ? 'rotate-180' : 'rotate-0'} transition-all`}>
 					<path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75l3 3m0 0l3-3m-3 3v-7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
 				</svg>
 			</div>
--- client/src/pages/web-seiten.jsx
+++ client/src/pages/web-seiten.jsx
@@ -33,11 +33,11 @@ export default function Home() {
 				<div className="relative overflow-hidden" id="hero">
 					<Image className="absolute top-0 w-full h-full object-cover" src={`${darkMode ? '/home/hero/wave/dark/wave.webp' : '/home/hero/wave/light/wave.webp'}`} fill={true} alt="" priority={true} />
 					<Navbar />
-					<section className="relative flex items-center justify-center xl:px-20 2xl:px-0 min-h-[80rem] xl:min-h-[50rem]" style={{height: 'calc(100vh - 5rem)', zIndex: '1'}}>
-						<div className="max-w-7xl flex flex-col xl:flex-row items-center gap-12 w-full">
-							<div className="flex flex-col gap-10 text-center xl:text-left xl:max-w-[33rem] 2xl:max-w-[37rem] px-20 xl:px-0">
+					<section className="relative flex items-center justify-center px-6 sm:px-10 lg:px-20 2xl:px-0 min-h-[57rem] sm:min-h-[55rem] md:min-h-[65rem] lg:min-h-[75rem] xl:min-h-[50rem]" style={{height: 'calc(100vh - 5rem)', zIndex: '1'}}>
+						<div className="max-w-7xl 2xl:max-w-[90rem] flex flex-col xl:flex-row items-center gap-12 2xl:gap-20 w-full">
+							<div className="flex flex-col gap-10 text-center xl:text-left xl:max-w-[33rem] 2xl:max-w-[40rem]">
 								<div className={`flex flex-col gap-5`}>
-									<h1 className={`${darkMode ? 'title-dark' : 'title-light'} text-5xl 2xl:text-6xl font-bold leading-[5rem] 2xl:leading-[5.5rem]`}>Maßgeschneiderte Webentwicklung für Ihr Unternehmen</h1>
+									<h1 className={`${darkMode ? 'title-dark' : 'title-light'} text-3xl leading-[4rem] md:text-4xl md:leading-[4.5rem] lg:text-5xl lg:leading-[5rem] 2xl:text-6xl 2xl:leading-[5.5rem] font-bold break-words`}>Maßgeschneiderte Webentwicklung für Ihr Unternehmen</h1>
 									<p className={`${darkMode ? 'description-dark font-light' : 'description-light'} 2xl:text-lg`}>Wir bieten maßgeschneiderte Lösungen, mit denen sich Ihr Unternehmen online von der Masse abhebt. Von Website-Design und -Entwicklung bis hin zur App-Programmierung und Suchmaschinenoptimierung kann unser Expertenteam Ihnen helfen, Ihre Online-Ziele zu erreichen.</p>
 								</div>
 								<div className="flex justify-center xl:justify-start">
@@ -54,25 +54,23 @@ export default function Home() {
 						</div>
 					</section>
 				</div>
-				<section className={`px-20 2xl:px-0 ${darkMode ? 'section-bg-dark' : 'section-bg-light'} py-28 overflow-hidden`} id="our-services">
-					<div className="max-w-7xl mx-auto z-10 relative">
+				<section className={`px-6 sm:px-10 lg:px-20 2xl:px-0 ${darkMode ? 'section-bg-dark' : 'section-bg-light'} py-28 overflow-hidden`} id="our-services">
+					<div className="max-w-7xl 2xl:max-w-[90rem] mx-auto z-10 relative">
 						<div className="flex items-center gap-20 justify-between w-full">
 							<div className="blur-shadow -left-28 -top-28"></div>
 							<div className="flex flex-col gap-20">
-								<div className="flex flex-col gap-5 relative">
+								<div className="flex flex-col items-center sm:items-start gap-5 relative">
 									<div className="flex flex-col">
 										<span className={`uppercase font-semibold ${darkMode ? 'subtitle-dark' : 'subtitle-light'}`}>Unsere Dienstleistungen</span>
 									</div>
-									<div className="flex items-start gap-20">
-										<div>
-											<h2 className={`flex flex-col gap-5 text-5xl font-bold whitespace-nowrap ${darkMode ? 'title-dark' : 'title-light'}`}>
-												<div>Welche</div>
-                                                <div className="text-primary">Dienstleistungen</div>
-												<div>{"wir anbieten"}</div>
-											</h2>
-										</div>
+									<div className="flex flex-col xl:flex-row justify-center sm:items-start gap-10 xl:gap-20">
+										<h2 className={`flex flex-col items-center sm:items-start gap-5 text-3xl sm:text-5xl font-bold whitespace-nowrap ${darkMode ? 'title-dark' : 'title-light'}`}>
+											<div>Welche</div>
+											<div className="text-primary">Dienstleistungen</div>
+											<div>{"wir anbieten"}</div>
+										</h2>
 										<div className={`flex flex-col gap-5 ${darkMode ? 'description-dark font-light' : 'description-light'}`}>
-											<p>Bei HelphisTech bieten wir eine breite Palette von Webentwicklungsdiensten an, um Unternehmen dabei zu helfen, eine starke Online-Präsenz zu schaffen und ihre digitalen Ziele zu erreichen. Unser Team aus erfahrenen Entwicklern ist in der Erstellung benutzerdefinierter Websites, Webanwendungen und E-Commerce-Plattformen geschult, die auf die individuellen Bedürfnisse jedes Kunden zugeschnitten sind.</p>
+											<p className="text-center sm:text-left">Bei HelphisTech bieten wir eine breite Palette von Webentwicklungsdiensten an, um Unternehmen dabei zu helfen, eine starke Online-Präsenz zu schaffen und ihre digitalen Ziele zu erreichen. Unser Team aus erfahrenen Entwicklern ist in der Erstellung benutzerdefinierter Websites, Webanwendungen und E-Commerce-Plattformen geschult, die auf die individuellen Bedürfnisse jedes Kunden zugeschnitten sind.</p>
 											{/* We use the latest technologies and industry best practices to ensure that our projects are of the highest quality, and we work closely with our clients to ensure that they are satisfied with the final product. */}
 										</div>
 									</div>
@@ -107,51 +105,53 @@ export default function Home() {
 						</div>
 					</div>
 				</section>
-				<section className={`relative overflow-hidden ${darkMode ? 'section-bg-dark border-[#19191F]' : 'section-bg-light border-zinc-300'} flex items-center py-28 px-20 2xl:px-0 border-t`} id="our-technologies">
-					<div className="blur-shadow -top-6 -left-6"></div>
-					<div className="flex flex-col gap-20 overflow-hidden max-w-7xl mx-auto">
-						<div className="flex flex-col gap-4 text-white relative">
-							<div className="flex flex-col gap-3">
-								<span className={`uppercase font-semibold ${darkMode ? 'subtitle-dark' : 'subtitle-light'}`}>Unsere Technologien</span>
-								<h2 className={`flex flex-col gap-5 text-5xl font-bold whitespace-nowrap ${darkMode ? 'title-dark' : 'title-light'}`}>
-									<div>Dies sind die <span className="text-primary">Technologien</span></div> 
-									<div>die wir am häufigsten verwenden</div>
-								</h2>
+				<section className={`relative overflow-hidden ${darkMode ? 'section-bg-dark border-[#19191F]' : 'section-bg-light border-zinc-300'} flex items-center py-28 px-6 sm:px-10 lg:px-20 2xl:px-0 border-t`} id="our-technologies">
+					<div className="max-w-7xl 2xl:max-w-[90rem] mx-auto w-full relative">
+						<div className="blur-shadow -top-6 -left-6"></div>
+						<div className="max-w-7xl 2xl:max-w-[90rem] mx-auto flex flex-col gap-20 overflow-hidden">
+							<div className="flex flex-col gap-4 text-white relative">
+								<div className="flex flex-col items-center sm:items-start text-center sm:text-left gap-5">
+									<span className={`uppercase font-semibold ${darkMode ? 'subtitle-dark' : 'subtitle-light'}`}>Unsere Technologien</span>
+									<h2 className={`flex flex-col items-center sm:items-start gap-2 sm:gap-3 text-3xl sm:text-5xl font-bold ${darkMode ? 'title-dark' : 'title-light'}`}>
+										<div className="flex flex-col md:flex-row gap-3 sm:gap-5 md:gap-2">Dies sind die <span className="text-primary">Technologien</span></div> 
+										<div className="leading-[3rem] sm:leading-[4rem]">die wir am häufigsten verwenden</div>
+									</h2>
+								</div>
+								<div className={`2xl:text-lg ${darkMode ? 'description-dark font-light' : 'description-light'}`}>
+									<p className="text-center sm:text-left">Die passende Technologie bereichert das Projekt. Hier sind einige von denen, die wir am häufigsten verwenden:</p>
+								</div>
 							</div>
-							<div className={`2xl:text-lg ${darkMode ? 'description-dark font-light' : 'description-light'}`}>
-								<p>Die passende Technologie bereichert das Projekt. Hier sind einige von denen, die wir am häufigsten verwenden:</p>
+							<div className="overflow-x-scroll hide-scroll">
+								<div className="grid grid-cols-5 gap-y-10 gap-x-14 xl:gap-x-28 select-none min-w-[60rem]">
+									<TechnologyImage src={"/technologies/react.webp"} alt={"React technology image"} />
+									<TechnologyImage src={"/technologies/nextjs.webp"} alt={"Next js technology image"} darkmode={"/technologies/darkmode/nextjs.webp"} />
+									<TechnologyImage src={"/technologies/angular.webp"} alt={"Angular technology image"} />
+									<TechnologyImage src={"/technologies/vuejs.webp"} alt={"Vue technology image"} />
+									<TechnologyImage src={"/technologies/astro.webp"} alt={"Astro technology image"} darkmode={"/technologies/darkmode/astro.webp"} />
+									<TechnologyImage src={"/technologies/tailwind.webp"} alt={"Tailwind technology image"} />
+									<TechnologyImage src={"/technologies/nodejs.webp"} alt={"Node js technology image"} darkmode={"/technologies/darkmode/nodejs.webp"} />
+									<TechnologyImage src={"/technologies/mongodb.webp"} alt={"MongoDB technology image"} />
+									<TechnologyImage src={"/technologies/mysql.webp"} alt={"MySQL technology image"} />
+									<TechnologyImage src={"/technologies/socketio.webp"} alt={"Socket.io technology image"} darkmode={"/technologies/darkmode/socketio.webp"} />
+								</div>
 							</div>
+							<Link className="flex justify-center" href={"#"}>
+								<div className="flex items-center gap-2 text-primary hover:text-primary-2 hover:underline transition-colors">
+									<div>Mehr Erfahren</div>
+									<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+										<path strokeLinecap="round" strokeLinejoin="round" d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3" />
+									</svg>
+								</div>
+							</Link>
 						</div>
-						<div className="overflow-x-scroll hide-scroll">
-							<div className="grid grid-cols-5 gap-y-10 gap-x-14 xl:gap-x-28 select-none min-w-[60rem]">
-								<TechnologyImage src={"/technologies/react.webp"} alt={"React technology image"} />
-								<TechnologyImage src={"/technologies/nextjs.webp"} alt={"Next js technology image"} darkmode={"/technologies/darkmode/nextjs.webp"} />
-								<TechnologyImage src={"/technologies/angular.webp"} alt={"Angular technology image"} />
-								<TechnologyImage src={"/technologies/vuejs.webp"} alt={"Vue technology image"} />
-								<TechnologyImage src={"/technologies/astro.webp"} alt={"Astro technology image"} darkmode={"/technologies/darkmode/astro.webp"} />
-								<TechnologyImage src={"/technologies/tailwind.webp"} alt={"Tailwind technology image"} />
-								<TechnologyImage src={"/technologies/nodejs.webp"} alt={"Node js technology image"} darkmode={"/technologies/darkmode/nodejs.webp"} />
-								<TechnologyImage src={"/technologies/mongodb.webp"} alt={"MongoDB technology image"} />
-								<TechnologyImage src={"/technologies/mysql.webp"} alt={"MySQL technology image"} />
-								<TechnologyImage src={"/technologies/socketio.webp"} alt={"Socket.io technology image"} darkmode={"/technologies/darkmode/socketio.webp"} />
-							</div>
-						</div>
-						<Link className="flex justify-center" href={"#"}>
-							<div className="flex items-center gap-2 text-primary hover:text-primary-2 hover:underline transition-colors">
-								<div>Mehr Erfahren</div>
-								<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-									<path strokeLinecap="round" strokeLinejoin="round" d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3" />
-								</svg>
-							</div>
-						</Link>
 					</div>
 				</section>
-				<section className="flex flex-col gap-16 py-28 text-center" id="start-my-project">
+				<section className="px-6 sm:px-10 lg:px-20 2xl:px-0 flex flex-col gap-16 py-28 text-center" id="start-my-project">
 					<div className="flex flex-col gap-8">
 						<div className={`${darkMode ? 'subtitle-dark' : 'subtitle-light'} text-lg uppercase font-semibold`}>
 							<span>Haben sie eine Idee?</span>
 						</div>
-						<div className="w-2/3 2xl:w-2/4 mx-auto">
+						<div className="lg:w-3/4 2xl:w-2/4 mx-auto">
 							<span className={`${darkMode ? 'title-dark' : 'title-light'} text-[2.25rem] sm:text-[3rem] md:text-[3.75rem] lg:text-[4.5rem] font-semibold`}>Wir brauchen nur eine Idee oder ein Problem, damit wir Ihr Projekt umsetzen können</span>
 						</div>
 					</div>
@@ -222,9 +222,9 @@ function ServicesOption({ title, description, href }) {
 
 	return (
 		<div onClick={handleShowDescription} className={`flex flex-col gap-5 py-9 cursor-pointer select-none ${darkMode ? 'subtitle-dark border-[#19191F] hover:text-zinc-200' : 'text-neutral-600 hover:text-black border-neutral-300'} transition-colors`}>
-			<div className={`flex items-center justify-between ${showDescription ? darkMode ? 'text-zinc-200' : 'text-black' : null}`}>
-				<h3 className="font-extralight text-4xl">{title}</h3>
-				<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={.7} stroke="currentColor" className={`w-12 h-12 ${showDescription ? 'rotate-180' : 'rotate-0'} transition-all`}>
+			<div className={`flex items-center justify-between gap-5 sm:gap-0 ${showDescription ? darkMode ? 'text-zinc-200' : 'text-black' : null}`}>
+				<h3 className="font-extralight text-xl sm:text-2xl lg:text-3xl xl:text-4xl">{title}</h3>
+				<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={.7} stroke="currentColor" className={`w-8 h-8 xl:w-12 xl:h-12 ${showDescription ? 'rotate-180' : 'rotate-0'} transition-all`}>
 					<path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75l3 3m0 0l3-3m-3 3v-7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
 				</svg>
 			</div>
