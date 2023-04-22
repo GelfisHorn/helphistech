7e74c12701fa8ceaa5e61b04db950e9f9570e7a9
GelfisHorn <helphis.tech@gmail.com>
2023-04-22 18:01:12 -0300

    Client: removed console.log in sendProject api.

--- client/src/pages/api/sendProject.js
+++ client/src/pages/api/sendProject.js
@@ -11,7 +11,6 @@ export default async function handler(req, res) {
 		return res.status(200).json(data);
 	} catch (error) {
 		const resError = new Error(error.response.data.msg)
-        console.log(error.response.data.msg)
 		return res.status(400).json({ msg: resError.message });
 	}
 }
