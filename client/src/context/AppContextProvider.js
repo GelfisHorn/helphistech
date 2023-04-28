6e8ec14185f69b8f7ed379519af2bd312db63f3f
GelfisHorn <helphis.tech@gmail.com>
2023-04-28 13:34:28 -0300

    Client: Comments on project.
    - Create comment.
    - Edit comment.
    - Delete comment.

--- client/src/context/AppContextProvider.js
+++ client/src/context/AppContextProvider.js
@@ -66,28 +66,6 @@ const AppContextProvider = ({children}) => {
         // Set theme on localStorage
         darkMode ? localStorage.setItem('darkmode', false) : localStorage.setItem('darkmode', true);
     }
-    
-    const [ showProjectQuote, setShowProjectQuote ] = useState(false);
-    // Show and hide Project Quote Menu
-    const handleShowProjectQuote = () => {
-        // Stop menu close animation
-        setProjectQuoteAnimation(false);
-        // Show/hide menu
-        setShowProjectQuote(current => !current);
-        // If menu is closed set overflow auto else set overflow hidden to prevent page scrolling
-        showProjectQuote ? document.body.style.overflowY = 'auto' : document.body.style.overflowY = 'hidden';
-    }
-
-    const [ projectQuoteAnimation, setProjectQuoteAnimation ] = useState(false);
-    // Apply delay on close Project Quote menu to see close animation
-    const handleCloseProjectQuote = () => {
-        // Show menu animation
-        setProjectQuoteAnimation(true);
-        // After 270ms close menu
-        setTimeout(() => {
-            handleShowProjectQuote();
-        }, 270)
-    }
 
     return (
         <AppContext.Provider value={{
@@ -97,12 +75,7 @@ const AppContextProvider = ({children}) => {
             language,
             setLanguage,
             handleDarkMode,
-            darkMode,
-            showProjectQuote,
-            handleShowProjectQuote,
-            projectQuoteAnimation,
-            setProjectQuoteAnimation,
-            handleCloseProjectQuote
+            darkMode
         }}>
             {children}
         </AppContext.Provider>
--- client/src/pages/admin/project/[project].jsx
+++ client/src/pages/admin/project/[project].jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 // React
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 // Nextjs
 import { useRouter } from "next/router"
 // Components
@@ -22,6 +22,7 @@ export default function ProjectDynamic() {
 
     // Project data
     const [ project, setProject ] = useState({});
+    const [ projectComments, setProjectComments ] = useState([]);
     const { website_type, contact_information, budget, description, state } = project;
 
     const [ projectState, setProjectState ] = useState(state);
@@ -48,9 +49,14 @@ export default function ProjectDynamic() {
             }
         }
 
-        const { data } = await axios.post(`/api/admin/getProject`, { _id: projectId, config });
-        setProject(data);
-        setProjectState(data.state)
+        try {
+            const { data } = await axios.post(`/api/admin/getProject`, { _id: projectId, config });
+            setProject(data.project);
+            setProjectComments(data.comments);
+            setProjectState(data.project.state)
+        } catch (error) {
+            console.log(error);
+        }
     }
 
     async function handleChangeState() {
@@ -143,8 +149,42 @@ export default function ProjectDynamic() {
         router.push(`/admin/projects`)
     }
 
+    const commentTextarea = useRef('');
+
+    async function handleSendComment(e) {
+        e.preventDefault();
+
+        const message = commentTextarea.current.value;
+        if(message == '') {
+            return;
+        }
+
+        // Get authentication token from localStorage
+        const token = localStorage.getItem('auth-token');
+        if(!token) {
+            setFetchingAuth(false);
+            return;
+        }
+
+        const config = {
+            headers: {
+                "Content-Type": "application-json",
+                Authorization: `Bearer ${token}`
+            }
+        }
+
+        try {
+            commentTextarea.current.value = '';
+            const comment = { userId: auth._id, projectId, message }
+            const { data } = await axios.post('/api/admin/projectComments/create', { config, comment });
+            setProjectComments(current => current.concat([data.comment]));
+        } catch (error) {
+            console.log(error.response.data.msg);
+        }
+    }
+
     return (
-        <Layout title={'Proyeto'}>
+        <Layout title={'Proyecto'}>
             { Object.keys(project).length != 0 && (
                 <div className={`${darkMode ? 'text-dark-text' : 'text-black'} flex flex-col gap-3 px-5 rounded-lg`}>
                     <div className={`${darkMode ? 'border-neutral-900' : 'border-neutral-200'} flex flex-col border-b py-3`}>
@@ -161,11 +201,15 @@ export default function ProjectDynamic() {
                         </div>
                         <div className={`${darkMode ? 'border-neutral-900' : 'border-neutral-200'} flex flex-col py-3 border-t`}>
                             <div className="font-bold uppercase">Presupuesto</div>
-                            <div className="flex items-center gap-1">
-                                <div className={`${darkMode ? 'text-zinc-400' : 'text-zinc-600'}`}>Entre</div>
-                                <div className="font-semibold">{currencyFormatter(budget.from)}</div>    
-                                <div className={`${darkMode ? 'text-zinc-400' : 'text-zinc-600'}`}>y</div>
-                                <div className="font-semibold">{currencyFormatter(budget.to)}</div>    
+                            <div className="flex flex-col xs:flex-row xs:items-center gap-1">
+                                <div className="flex items-center gap-1">
+                                    <div className={`${darkMode ? 'text-zinc-400' : 'text-zinc-600'}`}>Entre</div>
+                                    <div className="font-semibold">{currencyFormatter(budget.from)}</div>     
+                                </div>   
+                                <div className="flex items-center gap-1">
+                                    <div className={`${darkMode ? 'text-zinc-400' : 'text-zinc-600'}`}>y</div>
+                                    <div className="font-semibold">{currencyFormatter(budget.to)}</div>     
+                                </div>       
                             </div>    
                         </div>
                         <div className={`${darkMode ? 'border-neutral-900' : 'border-neutral-200'} flex flex-col gap-2 border-t py-3`}>
@@ -183,19 +227,19 @@ export default function ProjectDynamic() {
                                 </div>
                             </div>
                         </div>
-                        <div className={`${darkMode ? 'border-neutral-900' : 'border-neutral-200'} flex flex-row items-center gap-2 border-t pt-3`}>
+                        <div className={`${darkMode ? 'border-neutral-900' : 'border-neutral-200'} flex flex-col xs:flex-row xs:items-center gap-2 border-t pt-3`}>
                             <div className="uppercase font-semibold text-lg">Estado</div>
                             <div className={`${projectState == 'onhold' ? 'bg-yellow-500' : projectState == 'inprogress' ? 'bg-orange-500' : projectState == 'completed' ? 'bg-light-main' :  'bg-red-500'} w-fit px-4 py-1 rounded-full text-white uppercase font-semibold select-none transition-colors flex justify-center`}>
                                 <span>{projectState == 'onhold' ? 'En espera' : projectState == 'inprogress' ? 'En desarrollo' : projectState == 'completed' ? 'Completado' : 'Cancelado'}</span>
                             </div>
                         </div>
                     </div>
-                    <div className="flex flex-col sm:flex-row sm:items-end sm:justify-between gap-10 sm:gap-0">
+                    <div className="flex flex-col sm:flex-row sm:items-end sm:justify-between gap-10 sm:gap-0 pb-3">
                         <div className="flex flex-col gap-5">
                             <div className="flex flex-col gap-2">
                                 <div className="uppercase font-semibold text-lg">Cambiar Estado</div>
                                 <select value={projectState} onChange={(e) => handleChangeState(e.target.value)} className={`${darkMode ? 'bg-white text-black' : 'bg-black text-white'} py-1 rounded-lg outline-none w-fit px-4`}>
-                                    <option value="onhold">Pendiente</option>
+                                    <option value="onhold">En espera</option>
                                     <option value="inprogress">En desarrollo</option>
                                     <option value="completed">Completado</option>
                                 </select>
@@ -209,37 +253,211 @@ export default function ProjectDynamic() {
                             )}
                         </div>
                     </div>
+                    <div className={`flex flex-col gap-5 pt-16 pb-5 border-t ${darkMode ? 'border-neutral-900' : 'border-neutral-200'}`}>
+                        <div className={`flex flex-col gap-4 border-b ${darkMode ? 'border-neutral-900' : 'border-neutral-200'} pb-4`}>
+                            <div className="text-xl">Comentarios</div>
+                            <form className="flex flex-col gap-2" onSubmit={handleSendComment}>
+                                <textarea 
+                                    ref={commentTextarea}
+                                    className={`bg-transparent border ${darkMode ? 'border-neutral-900 placeholder:text-neutral-500' : 'border-neutral-200 placeholder:text-neutral-300'} w-full px-3 py-2 resize-none outline-none`} 
+                                    placeholder="Haz un comentario" 
+                                    rows="3">
+                                </textarea>
+                                <div className="flex justify-end">
+                                    <button type="submit" className="py-2 px-6 bg-primary text-white uppercase rounded-sm font-medium">Comentar</button>
+                                </div>
+                            </form>
+                        </div>
+                        <div className="flex flex-col gap-2">
+                            {projectComments.map((comment, index) => (
+                                <ProjectComment 
+                                    key={index}
+                                    comment={comment}
+                                    comments={projectComments}
+                                    setComments={setProjectComments}
+                                />
+                            ))}
+                        </div>
+                    </div>
                 </div>
             )}
             { showCancelMenu && 
-                <CancelProjectMenu 
-                    closeAnim={closeCancelMenuAnim} 
-                    confirm={projectState != 'cancelled' ? handleCancelProject : handleRecoverProject} 
-                    cancel={handleCloseModal} 
-                    projectState={projectState} 
-                />
+                <Modal showModal={showCancelMenu}>
+                    <div className="flex flex-col gap-1">
+                        <div className={`${projectState != 'cancelled' ? 'text-red-700' : 'text-light-main'} text-xl font-semibold uppercase`}>{projectState != 'cancelled' ? 'Cancelar proyecto' : 'Recuperar proyecto'}</div>
+                        <div className="text-lg">¿Estás seguro que deseas {projectState != 'cancelled' ? 'cancelar' : 'recuperar'} este proyecto?</div>
+                    </div>
+                    <div className="flex items-center justify-between">
+                        <button className="bg-red-500 hover:bg-red-900 text-white rounded-md px-4 py-2 uppercase font-semibold transition-colors" onClick={() => setShowCancelMenu(false)}>Cancelar</button>
+                        <button className="bg-light-main hover:bg-indigo-900 text-white rounded-md px-4 py-2 uppercase font-semibold transition-colors" onClick={projectState == 'cancelled' ? handleRecoverProject : handleCancelProject}>Confirmar</button>
+                    </div>
+                </Modal>
             }
         </Layout>
     )
 }
 
-function CancelProjectMenu({ closeAnim, confirm, cancel, projectState }) {
+function ProjectComment({ comment, comments, setComments }) {
+    
+    const { darkMode, auth } = useContextProvider();
+    
+    const { _id, user, message, createdAt } = comment;
+
+    // Edit comment state
+    const [ editingComment, setEditingComment ] = useState(false);
+    // textarea value
+    const [ textArea, setTextArea ] = useState(message);
+    function handleEditingComment() {
+        setEditingComment(!editingComment);
+    }
+    // On click "guardar" edit comment
+    async function handleEditComment() {
+        handleEditingComment();
+
+        // Get authentication token from localStorage
+        const token = localStorage.getItem('auth-token');
+        if(!token) {
+            setFetchingAuth(false);
+            return;
+        }
+
+        const config = {
+            headers: {
+                "Content-Type": "application-json",
+                Authorization: `Bearer ${token}`
+            }
+        }
+
+        try {
+            await axios.post('/api/admin/projectComments/edit', { commentId: _id, message: textArea, config });
+            const newComments = comments.map(comment => {
+                if(comment._id == _id) {
+                    comment.message = textArea;
+                    return comment;
+                }
+                return comment;
+            })
+            setComments(newComments);
+        } catch (error) {
+            console.log(error.response.data.msg)
+        }
+    }
+
+    // Delete comment state
+    const [ showModal, setShowModal ] = useState(false);
+    const handleShowModal = () => {
+        setShowModal(!showModal);
+    }
+    // On click "confirm" in modal, delete comment.
+    async function handleDeleteComment() {
+        handleShowModal();
+
+        // Get authentication token from localStorage
+        const token = localStorage.getItem('auth-token');
+        if(!token) {
+            setFetchingAuth(false);
+            return;
+        }
+
+        const config = {
+            headers: {
+                "Content-Type": "application-json",
+                Authorization: `Bearer ${token}`
+            }
+        }
+
+        try {
+            await axios.post('/api/admin/projectComments/delete', { commentId: _id, config });
+            const newComments = comments.filter(comment => comment._id != _id);
+            setComments(newComments);
+        } catch (error) {
+            console.log(error.response.data.msg)
+        }
+    }
+
+
+    return (
+        <div className={`flex flex-col px-5 py-4 rounded-md shadow-md ${darkMode ? 'bg-[#101010]' : 'bg-zinc-100'}`}>
+            <div className="flex items-center justify-between">
+                <div className={`${darkMode ? 'text-neutral-300' : 'text-neutral-700'}`}>{user.name}:</div>
+                { auth._id === user._id && (
+                    <div className="flex items-center gap-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="cursor-pointer w-7 h-7 p-1 hover:bg-neutral-700 rounded-md transition-colors" onClick={handleEditingComment}>
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
+                        </svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="cursor-pointer w-7 h-7 p-1 hover:bg-red-700 rounded-md transition-colors" onClick={handleShowModal}>
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                        </svg>
+                    </div>
+                )}
+            </div>
+            { editingComment ? (
+                <div className="flex flex-col gap-2 mt-1 mb-3">
+                    <textarea 
+                        value={textArea}
+                        onChange={(e) => setTextArea(e.target.value)}
+                        className={`bg-transparent border ${darkMode ? 'border-neutral-900 placeholder:text-neutral-500' : 'border-neutral-200 placeholder:text-neutral-300'} w-full px-3 py-2 resize-none outline-none`} 
+                        placeholder="Haz un comentario" 
+                        rows="3">
+                    </textarea>
+                    <div className="flex justify-end gap-2">
+                        <button onClick={handleEditingComment} className="py-1 px-3 bg-red-500 hover:bg-red-800 transition-colors rounded-sm">Cancelar</button>
+                        <button onClick={handleEditComment} className="py-1 px-3 bg-primary hover:bg-primary-2 transition-colors rounded-sm">Guardar</button>
+                    </div>
+                </div>
+            ) : (
+                <div className="break-words">{message}</div>
+            )}
+            <div className={`text-right text-sm font-semibold ${darkMode ? 'text-neutral-400' : 'text-neutral-600'}`}>{moment(createdAt).format('LLL')}</div>
+            <Modal showModal={showModal}>
+                <div className="flex flex-col gap-1">
+                    <div className={`text-red-500 text-xl font-semibold uppercase`}>Eliminar comentario</div>
+                    <div className="text-lg">¿Estás seguro que deseas eliminar este comentario?</div>
+                </div>
+                <div className="flex items-center justify-between">
+                    <button className="bg-red-500 hover:bg-red-900 text-white rounded-md px-4 py-2 uppercase font-semibold transition-colors" onClick={handleShowModal}>Cancelar</button>
+                    <button className="bg-light-main hover:bg-indigo-900 text-white rounded-md px-4 py-2 uppercase font-semibold transition-colors" onClick={handleDeleteComment}>Confirmar</button>
+                </div>
+            </Modal>
+        </div>
+    )
+}
+
+function Modal({ showModal, children }) {
 
     const { darkMode } = useContextProvider();
 
+    const [ show, setShow ] = useState(false);
+    const [ closeAnim, setCloseAnim ] = useState(false);
+
+    function handleShowModal() {
+        setShow(true);
+    }
+
+    function handleCloseModal() {
+        setCloseAnim(true);
+        setTimeout(() => {
+            setCloseAnim(false);
+            setShow(false);
+        }, 170)
+    }
+
+    useEffect(() => {
+        if(showModal) {
+            handleShowModal();
+            return;
+        }
+        handleCloseModal();
+    }, [showModal])
+
     return (
-        <>
-            <div className="fixed bg-black opacity-75 top-0 left-0 w-screen h-screen"></div>
-            <div className={`${darkMode ? 'bg-neutral-900 text-dark-text' : 'bg-white text-black'} ${closeAnim ? 'modal-close' : 'modal-open'} fixed top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 flex flex-col gap-7 shadow-md px-5 py-4 rounded-md`}>
-                <div className="flex flex-col gap-1">
-                    <div className={`${projectState != 'cancelled' ? 'text-red-700' : 'text-light-main'} text-xl font-semibold uppercase`}>{projectState != 'cancelled' ? 'Cancelar proyecto' : 'Recuperar proyecto'}</div>
-                    <div className="text-lg">¿Estás seguro que deseas {projectState != 'cancelled' ? 'cancelar' : 'recuperar'} este proyecto?</div>
+        show && (
+            <>
+                <div className="fixed bg-black opacity-75 top-0 left-0 w-screen h-screen"></div>
+                <div className={`${darkMode ? 'bg-neutral-900 text-dark-text' : 'bg-white text-black'} ${closeAnim ? 'modal-close' : 'modal-open'} fixed top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 flex flex-col gap-7 shadow-md px-5 py-4 rounded-md`}>
+                    {children}
                 </div>
-                <div className="flex items-center justify-between">
-                    <button className="bg-red-500 hover:bg-red-900 text-white rounded-md px-4 py-2 uppercase font-semibold transition-colors" onClick={cancel}>Cancelar</button>
-                    <button className="bg-light-main hover:bg-indigo-900 text-white rounded-md px-4 py-2 uppercase font-semibold transition-colors" onClick={confirm}>Confirmar</button>
-                </div>
-            </div>
-        </>
+            </>
+        )
     )
 }
