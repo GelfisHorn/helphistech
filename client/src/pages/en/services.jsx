18279433eff9f7870ede55564d636feebc53d2c3
GelfisHorn <helphis.tech@gmail.com>
2023-05-02 11:26:56 -0300

    Client: Services page changes.
    - Added an image and text to each service.

--- client/src/pages/en/services.jsx
+++ client/src/pages/en/services.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 // Nextjs
 import Link from "next/link";
+import Image from "next/image";
 // Components
 import Layout from "@/components/Layout";
 // Context
@@ -13,40 +14,51 @@ export default function ServicesPage() {
 
     return (
         <Layout title={"Our services"} lang={'en'}>
-            <main className={`px-6 sm:px-10 lg:px-20 2xl:px-0 py-20 overflow-hidden`}>
-                <div className="max-w-7xl 2xl:max-w-[90rem] mx-auto z-10 relative">
+            <main className={`overflow-hidden`}>
+                <div className="z-10 relative">
                     <div className="flex items-center justify-between w-full">
-                        <div className="flex flex-col gap-[4.5rem]">
-                            <div className="flex items-center sm:items-start gap-5 relative">
+                        <div className="flex flex-col">
+                            <div className="flex items-center sm:items-start gap-5 relative px-6 sm:px-10 lg:px-20 2xl:px-40 py-28 2xl:py-36">
                                 <div className="flex flex-col justify-center sm:items-start gap-10">
                                     <div className={`text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-medium h-fit lg:leading-[4rem]`}>
                                         <h1 className="text-center sm:text-left w-full">What Services We're Offering</h1>
                                     </div>
                                     <div className={`flex flex-col gap-5 ${darkMode ? 'description-dark font-light' : 'description-light'}`}>
                                         <p className="text-center sm:text-left">At HelphisTech, we offer a wide range of web development services to help businesses create a strong online presence and achieve their digital goals. Our team of experienced developers is skilled in creating custom websites, web applications, and e-commerce platforms that are tailored to meet the unique needs of each client.</p>
-                                        {/* We use the latest technologies and industry best practices to ensure that our projects are of the highest quality, and we work closely with our clients to ensure that they are satisfied with the final product. */}
                                     </div>
                                 </div>
                             </div>
-                            <div className={`flex flex-col divide-y`}>
+                            <div className={`flex flex-col divide-y bg-[#080808] px-6 sm:px-10 lg:px-20 2xl:px-40`}>
                                 <ServicesOption
                                     title={"Custom Website Development"}
-                                    description={"We specialize in creating custom websites that are designed to meet the specific needs of our clients. Our websites are responsive, user-friendly, and optimized for search engines to ensure maximum visibility."}
+                                    p1={"This service focuses on designing and developing unique, customized websites that cater to the specific needs of your clients. By working closely with your clients, you can create a website that reflects their brand, values, and business goals."}
+                                    p2={"From graphic design and information architecture, to coding and search engine optimization, custom website development ensures that your clients' website is attractive, functional, and effective."}
+                                    image={"/services/custom.webp"}
+                                    alt={"Custom website development"}
                                     side={'left'}
                                 />
                                 <ServicesOption
                                     title={"Web Application Development"}
-                                    description={"We can develop complex web applications that are designed to streamline your business processes and improve efficiency. Our team has expertise in various programming languages and frameworks, including React, Angular, and Node."}
+                                    p1={"This service focuses on creating custom web applications that provide a specific technological solution for your clients' needs. This can include applications for project management, collaboration tools, sales tracking systems, among others."}
+                                    p2={"By working closely with your clients, you can design and develop a web application that meets their specific requirements and enhances the efficiency of their business."}
+                                    image={"/services/application.webp"}
+                                    alt={"Web Application Development"}
                                     side={'right'}
                                 />
                                 <ServicesOption
                                     title={"E-Commerce Development"}
-                                    description={"We can create custom e-commerce platforms that are designed to help businesses sell their products and services online. Our e-commerce websites are secure, easy to use, and can integrate with popular payment gateways such as PayPal and Stripe."}
+                                    p1={"This service focuses on creating customized online stores for your clients, providing an easy and attractive online shopping experience for end users."}
+                                    p2={"From creating product catalogs, integrating secure payment systems, to creating inventory and logistics management systems, E-Commerce development is a comprehensive solution for clients looking to expand their business online."}
+                                    image={"/services/ecommerce.webp"}
+                                    alt={"E-Commerce Development"}
                                     side={'left'}
                                 />
                                 <ServicesOption
                                     title={"Website Maintenance and Support"}
-                                    description={"We provide ongoing maintenance and support for all of our websites and web applications. Our team is available to troubleshoot any issues that may arise and to ensure that your website is always up-to-date and running smoothly."}
+                                    p1={"This service focuses on maintaining and improving the performance of your clients' existing websites. By providing security updates, content updates, and regular maintenance, website maintenance and support ensures that your clients' website is always up-to-date, secure, and running smoothly."}
+                                    p2={"Additionally, this service may include troubleshooting technical issues and implementing improvements to increase the performance and efficiency of your clients' website."}
+                                    image={"/services/maintenance.webp"}
+                                    alt={"Maintenance and Support"}
                                     side={'right'}
                                 />
                             </div>
@@ -58,35 +70,43 @@ export default function ServicesPage() {
     )
 }
 
-function ServicesOption({ title, description, side }) {
+function ServicesOption({ title, p1, p2, image, alt, side }) {
 
     // Get functions and variables from context
 	const { darkMode } = useContextProvider();
 
 	return (
-		<div className={`${side == 'left' ? 'items-start text-left' : 'items-end text-right'} flex flex-col gap-5 py-20 ${darkMode ? 'subtitle-dark border-[#19191F]' : 'text-neutral-600 border-neutral-300'} transition-colors`}>
-			<div className={`flex items-center justify-between gap-5 sm:gap-0 ${darkMode ? 'text-zinc-200' : 'text-black'}`}>
-				<h3 className="font-extralight text-xl sm:text-2xl lg:text-3xl xl:text-4xl text-primary">{title}</h3>
-			</div>
-			<div className={`${side == 'left' ? 'items-start text-left' : 'items-end text-right'} flex flex-col gap-5`}>
-                <p className={`font-light ${darkMode ? 'description-dark' : 'description-light'}`}>{description}</p>
-                <Link className="text-primary hover:text-primary-2 hover:underline transition-colors" href={"/en/contact"}>
-                    <div className="flex items-center gap-2">
-                        { side == 'right' && (
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18" />
-                            </svg>
-                            
-                        )}
-                        <div>I'm interested</div>
-                        { side == 'left' && (
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3" />
-                            </svg>
-                        )}
+		<div className={`flex items-center ${side == 'left' ? 'flex-col-reverse lg:flex-row' : 'flex-col-reverse lg:flex-row-reverse'} gap-10 lg:gap-20 py-20 ${darkMode ? 'subtitle-dark border-[#19191F]' : 'text-neutral-600 border-neutral-300'}`}>
+            <div className={`${side == 'left' ? 'items-center text-center lg:items-start lg:text-left' : 'items-center text-center lg:items-end lg:text-right'} flex flex-col gap-5 transition-colors`}>
+                <div className={`flex items-center justify-between gap-5 sm:gap-0 ${darkMode ? 'text-zinc-200' : 'text-black'}`}>
+                    <h3 className="font-extralight text-2xl xs:text-3xl md:text-4xl text-primary">{title}</h3>
+                </div>
+                <div className={`${side == 'left' ? 'items-center text-center lg:items-start lg:text-left' : 'items-center text-center lg:items-end lg:text-right'} flex flex-col gap-5`}>
+                    <div className="flex flex-col gap-3">
+                        <p className={`font-light ${darkMode ? 'description-dark' : 'description-light'}`}>{p1}</p>
+                        <p className={`font-light ${darkMode ? 'description-dark' : 'description-light'}`}>{p2}</p>
                     </div>
-                </Link>
+                    <Link className="text-primary hover:text-primary-2 hover:underline transition-colors" href={"/es/contacto"}>
+                        <div className={`flex items-center gap-2 ${side == 'right' ? `flex-row-reverse lg:flex-row` : ''}`}>
+                            { side == 'right' && (
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={`w-6 h-6 rotate-180 lg:rotate-0`}>
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18" />
+                                </svg>
+                                
+                            )}
+                            <div>I'm interested</div>
+                            { side == 'left' && (
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3" />
+                                </svg>
+                            )}
+                        </div>
+                    </Link>
+                </div>
             </div>
-		</div>
+            <div className="">
+                <Image className="max-w-[15rem] xs:max-w-[20rem] 2xl:max-w-[25rem]" src={image} width={1000} height={1000} alt={alt} priority />
+            </div>
+        </div>
 	)
 }
