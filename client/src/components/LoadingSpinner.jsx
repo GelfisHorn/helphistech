b101c28d293a4c45d56fee95790955a8c98ee6bb
GelfisHorn <helphis.tech@gmail.com>
2023-05-02 15:26:08 -0300

    Loading spinner syntax fix.

--- client/src/components/LoadingSpinner.jsx
+++ client/src/components/LoadingSpinner.jsx
@@ -1,13 +1,13 @@
 export default function LoadingSpinner() {
 
     return (
-        <div class="sk-chase">
-            <div class="sk-chase-dot"></div>
-            <div class="sk-chase-dot"></div>
-            <div class="sk-chase-dot"></div>
-            <div class="sk-chase-dot"></div>
-            <div class="sk-chase-dot"></div>
-            <div class="sk-chase-dot"></div>
+        <div className="sk-chase">
+            <div className="sk-chase-dot"></div>
+            <div className="sk-chase-dot"></div>
+            <div className="sk-chase-dot"></div>
+            <div className="sk-chase-dot"></div>
+            <div className="sk-chase-dot"></div>
+            <div className="sk-chase-dot"></div>
         </div>
     )
 }
