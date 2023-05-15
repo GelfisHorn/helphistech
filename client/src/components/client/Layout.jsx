888e9f5ceeb9274b86b0836016d59d52803d3c7f
GelfisHorn <helphis.tech@gmail.com>
2023-05-15 10:35:32 -0300

    Client panel layout

---
+++ client/src/components/client/Layout.jsx
@@ -0,0 +1,37 @@
+import { useEffect } from "react";
+// Nextjs
+import Head from "next/head"
+import { useRouter } from "next/router"
+// Components
+import Navbar from "./Layout/Navbar";
+import Sidebar from "./Layout/Sidebar";
+// Context
+import useContextProvider from "@/hooks/useAppContextProvider"
+
+export default function ClientLayout({ children, title }) {
+    
+    const { darkMode, auth } = useContextProvider();
+
+    const router = useRouter();
+
+    return (
+        <>
+            <Head>
+                <title>{title}</title>
+            </Head>
+            <div className={darkMode ? 'bg-darkmode' : 'bg-white'}>
+                <div>
+                    <Navbar />
+                </div>
+                <div className="flex overflow-hidden">
+                    <div>
+                        <Sidebar />
+                    </div>
+                    <div className="w-full py-6 px-2 sm:px-4 lg:px-8 overflow-y-scroll lazy-load-1" style={{height: 'calc(100vh - 3.5rem)'}}>
+                        {children}
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
