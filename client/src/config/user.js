f93bbc5a303b00d6d8bc0422f533e5173c759601
GelfisHorn <helphis.tech@gmail.com>
2023-05-17 00:34:25 -0300

    Create account form: config file

---
+++ client/src/config/user.js
@@ -0,0 +1,20 @@
+const POSITION = {
+    // "superadmin": "Superadmin",
+    "admin": "Admin",
+    "client": "Client",
+    "full-stack": "Full-stack Developer",
+    "front-end": "Front-end Developer",
+    "back-end": "Back-end Developer"
+}
+
+const PERMISSIONS = {
+    // "superadmin": "Superadmin",
+    "admin": "Admin",
+    "client": "Client",
+    "developer": "Developer"
+}
+
+export {
+    POSITION,
+    PERMISSIONS
+}
