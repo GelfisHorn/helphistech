a81ae2a6f446cdf74af23f817420332ad5e2a265
GelfisHorn <helphis.tech@gmail.com>
2023-05-19 07:57:02 -0300

    Installed Google Tag

--- client/src/pages/_document.jsx
+++ client/src/pages/_document.jsx
@@ -1,10 +1,22 @@
+import { useEffect } from 'react'
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
+
+	useEffect(() => {
+		window.dataLayer = window.dataLayer || []
+		function gtag(){dataLayer.push(arguments)}
+		gtag('js', new Date())
+
+		gtag('config', 'G-M2B027NX0L')
+	}, [])
+
 	return (
 		<Html lang="en">
 			<Head>
 				{/* <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v6.4.0/css/all.css" /> */}
+				{/* Google tag (gtag.js) */}
+				<script async src="https://www.googletagmanager.com/gtag/js?id=G-M2B027NX0L"></script>
 			</Head>
 			<body className='overflow-x-hidden'>
 				<Main />
