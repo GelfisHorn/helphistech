896cb3ae00700eaf507a6fe0725ddd17b5e5a8ad
GelfisHorn <helphis.tech@gmail.com>
2023-05-23 12:33:48 -0300

    Created showToast hook

---
+++ client/src/hooks/showToast.js
@@ -0,0 +1,68 @@
+import { toast } from 'react-toastify';
+
+/**
+ * Show react toastify notification 
+ * @text notification container (string)
+ * @type Notification types: "info", "success", "warning", "error". (Leave blank for default type)
+ */
+
+const showToast = (text, type) => {
+
+    const TYPES = {
+        "info": () => toast.info(text, {
+            position: "top-right",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "dark",
+        }),
+        "success": () => toast.success(text, {
+            position: "top-right",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "dark",
+        }),
+        "warning": () => toast.warn(text, {
+            position: "top-right",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "dark",
+        }),
+        "error": () => toast.error(text, {
+            position: "top-right",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "dark",
+        })
+    }
+    
+    type ? TYPES[type]() : (
+        toast(text, {
+            position: "top-right",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "dark",
+        })
+    )
+} 
+
+export default showToast;
