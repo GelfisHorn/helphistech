5121b5e832366b3b4b9008c2eaa2c0e86cce2298
GelfisHorn <helphis.tech@gmail.com>
2023-05-30 10:30:43 -0300

    getEntries nextjs api fix

--- client/src/pages/api/client/project/entry/getEntries.js
+++ client/src/pages/api/client/project/entry/getEntries.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 export default async function handler(req, res) {
-	const { config } = req.body;
+	const { project, config } = req.body;
 
 	try {
-		const { data } = await axios(`${process.env.SERVER_URI}/v1/client/project/entry`, config);
+		const { data } = await axios(`${process.env.SERVER_URI}/v1/client/project/entry/${project}`, config);
 		return res.status(200).json(data);
 	} catch (error) {
 		const resError = new Error(error.response.data.msg)
