0b11b9ae9bb51dd5f43b2c40dc0074f4cd601fcc
GelfisHorn <helphis.tech@gmail.com>
2023-05-31 11:51:39 -0300

    deleteImages file update
    Delete files from custom folder

--- client/src/hooks/deleteImages.js
+++ client/src/hooks/deleteImages.js
@@ -5,20 +5,21 @@ import sha1 from 'sha1';
  * Delete images in Cloudinary
  * @images formlist of images
  */
-export default async function deleteImages(images) {
+export default async function deleteImages(images, folder) {
     for(const image of images) {
-        const public_id = image.split('/')[7].split('.')[0];
+        const urlPieces = image.split('/');
+        const public_id = urlPieces[urlPieces.length - 1].split('.')[0];
         const timestamp = Math.floor(Date.now() / 1000);
         const api_key = process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY;  
         const api_secret = process.env.NEXT_PUBLIC_CLOUDINARY_API_SECRET;
-        const signature = sha1(`public_id=${public_id}&timestamp=${timestamp}` + api_secret)
+        const signature = sha1(`public_id=${folder ? `${folder}/${public_id}` : public_id}&timestamp=${timestamp}` + api_secret)
         let formData = new FormData();
-        formData.append('public_id', public_id);
+        formData.append('public_id', `${folder ? `${folder}/${public_id}` : public_id}`);
         formData.append('signature', signature);
         formData.append('api_key', api_key);
         formData.append('timestamp', timestamp);
         try {
-            const { data } = await axios.request({
+            await axios.request({
                 url: `https://api.cloudinary.com/v1_1/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/destroy`,
                 maxBodyLength: Infinity,
                 method: 'POST',
