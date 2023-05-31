37fa21286061ceb09e2b7bd614d0c70a47a62c60
GelfisHorn <helphis.tech@gmail.com>
2023-05-31 11:55:49 -0300

    update endpoint url

--- client/src/pages/api/client/project/entry/getEntries.js
+++ client/src/pages/api/client/project/entry/getEntries.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
 	const { project, config } = req.body;
 
 	try {
-		const { data } = await axios(`${process.env.SERVER_URI}/v1/client/project/entry/${project}`, config);
+		const { data } = await axios(`${process.env.SERVER_URI}/v1/client/project/entry/all/${project}`, config);
 		return res.status(200).json(data);
 	} catch (error) {
 		const resError = new Error(error.response.data.msg)
