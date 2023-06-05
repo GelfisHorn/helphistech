afdc0183ce5e7990d7d7d64baf56d25db3359259
GelfisHorn <helphis.tech@gmail.com>
2023-06-05 18:40:25 -0300

    Api for "Legal" pages

---
+++ client/src/pages/api/impressum.js
@@ -0,0 +1,26 @@
+import axios from "axios"
+
+export default async function handler(req, res) {
+
+    const { language } = req.body || {};
+
+    const config = {
+        headers: {
+            Authorization: `bearer ${process.env.STRAPI_TOKEN}`
+        }
+    }
+
+    try {
+        const { data } = await axios.request({
+            url: `${process.env.STRAPI_URI}/api/impressum?locale=${language}`,
+            method: "GET",
+            headers: {
+                'Authorization': config.headers.Authorization
+            }
+        })
+        return res.status(200).json(data.data.attributes);
+    } catch (error) {
+        const resError = new Error(error.response.data.msg)
+        return res.status(400).json({ msg: resError.message });
+    }
+}
