b1ee63b911be5be188e90dcd0712a048c204c632
GelfisHorn <helphis.tech@gmail.com>
2023-06-08 15:59:36 -0300

    Meta tag for google analytics

--- client/src/pages/_document.jsx
+++ client/src/pages/_document.jsx
@@ -3,20 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
 
-	useEffect(() => {
-		window.dataLayer = window.dataLayer || []
-		function gtag(){dataLayer.push(arguments)}
-		gtag('js', new Date())
-
-		gtag('config', 'G-M2B027NX0L')
-	}, [])
-
 	return (
-		<Html lang="en">
+		<Html lang="de">
 			<Head>
-				{/* <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v6.4.0/css/all.css" /> */}
-				{/* Google tag (gtag.js) */}
-				<script async src="https://www.googletagmanager.com/gtag/js?id=G-M2B027NX0L"></script>
+				<meta name="google-site-verification" content="N2tEqjmEa6dAKEmjfJlEc705r01Z1pjwGHm89yhYmYk" />
 			</Head>
 			<body className='overflow-x-hidden'>
 				<Main />
