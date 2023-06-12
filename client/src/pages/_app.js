e5e01ff1c0446fe181cd9a8ea5c5b8cae000bef0
GelfisHorn <helphis.tech@gmail.com>
2023-06-12 10:58:19 -0300

    Cookies floating bar

--- client/src/pages/_app.js
+++ client/src/pages/_app.js
@@ -2,14 +2,16 @@
 import { AppContextProvider } from '../context/AppContextProvider';
 // Vercel analytics
 import { Analytics } from '@vercel/analytics/react' ;
-
 import '@/styles/globals.css'
+// Components
+import CookiesBar from '@/components/CookiesBar'
 
 export default function App({ Component, pageProps }) {
 	return (
 		<AppContextProvider>
 			<Component {...pageProps} />
 			<Analytics />
+			<CookiesBar />
 		</AppContextProvider>
 	)
 }
