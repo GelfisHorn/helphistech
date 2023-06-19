ad92b3453c0555218c4379eca0df4bacc760d584
Gelfis Horn <130309879+GelfisHorn@users.noreply.github.com>
2023-06-19 13:54:14 +0200

    Added google tag (#86)

--- client/src/pages/_document.jsx
+++ client/src/pages/_document.jsx
@@ -3,11 +3,20 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
 
+	useEffect(() => {
+		window.dataLayer = window.dataLayer || [];
+		function gtag() { dataLayer.push(arguments); }
+		gtag('js', new Date());
+
+		gtag('config', 'AW-11148352893');
+	}, [])
+
 	return (
 		<Html lang="de">
 			<Head>
 				<meta name="google-site-verification" content="N2tEqjmEa6dAKEmjfJlEc705r01Z1pjwGHm89yhYmYk" />
 				<link href="/favicon.ico" rel="shortcut icon" type="image/x-icon" />
+				<script async src="https://www.googletagmanager.com/gtag/js?id=AW-11148352893"></script>
 			</Head>
 			<body className='overflow-x-hidden'>
 				<Main />
