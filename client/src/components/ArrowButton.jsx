70d4850c76bf7d1f2db76f7525df7f9cbe875505
GelfisHorn <helphis.tech@gmail.com>
2023-06-22 15:22:14 -0300

    hero section first design (mobile)

--- client/src/components/ArrowButton.jsx
+++ client/src/components/ArrowButton.jsx
@@ -25,29 +25,29 @@ const toRightVar = {
     }
 };
 
-export default function Button({ text, toLeft, toRight, link, classes }) {
+export default function Button({ text, toLeft, toRight, link, classes, width }) {
     return (
         link ? (
-            <Link href={link} className={"w-fit"}>
-                <motion.div initial="rest" whileHover="hover" animate="rest" className={`flex items-center gap-1 bg-primary hover:bg-primary-2 transition-colors text-white w-fit ${classes}`}>
-                    {toRight && (
-                        <>
-                            <span>{text}</span>
-                            <motion.svg variants={toRightVar} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-7 h-7">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3" />
-                            </motion.svg>
-                        </>
-                    )}
-                    {toLeft && (
-                        <>
-                            <motion.svg variants={toLeftVar} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18" />
-                            </motion.svg>
-                            <span>{text}</span>
-                        </>
-                    )}
-                </motion.div>
-            </Link>
+            <motion.div initial="rest" whileHover="hover" animate="rest" className={width}>
+                <Link href={link} className={`flex items-center gap-1 w-full bg-primary hover:bg-primary-2 transition-colors text-white ${classes}`}>
+                        {toRight && (
+                            <>
+                                <span>{text}</span>
+                                <motion.svg variants={toRightVar} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-7 h-7">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3" />
+                                </motion.svg>
+                            </>
+                        )}
+                        {toLeft && (
+                            <>
+                                <motion.svg variants={toLeftVar} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18" />
+                                </motion.svg>
+                                <span>{text}</span>
+                            </>
+                        )}
+                </Link>
+            </motion.div>
         ) : (
             <div className={"w-fit cursor-pointer"}>
                 <motion.div initial="rest" whileHover="hover" animate="rest" className={`flex items-center gap-1 bg-primary hover:bg-primary-2 transition-colors text-white w-fit ${classes}`}>
--- client/src/components/HomeSections/Hero.jsx
+++ client/src/components/HomeSections/Hero.jsx
@@ -9,6 +9,7 @@ import Navbar from "@/components/Navbar";
 import ContactModal from "../Modals/Contact/Index";
 // Animations
 import { motion, AnimatePresence } from "framer-motion";
+import Button from "../ArrowButton";
 
 export default function HeroSection() {
     
@@ -22,35 +23,70 @@ export default function HeroSection() {
 	}
 
     return (
-        <div className="relative overflow-hidden" id="hero">
-            <Image className="absolute top-0 w-full h-full object-cover" src={`${darkMode ? '/home/hero/wave/dark/wave.webp' : '/home/hero/wave/light/wave.webp'}`} fill={true} alt="" priority />
-            <Navbar />
-            <section className="relative flex items-center justify-center px-6 sm:px-10 lg:px-20 2xl:px-0 min-h-[57rem] sm:min-h-[55rem] md:min-h-[65rem] lg:min-h-[75rem] xl:min-h-[50rem]" style={{height: 'calc(100vh - 5rem)', zIndex: '1'}}>
-                <div className="max-w-7xl 2xl:max-w-[90rem] flex flex-col xl:flex-row items-center gap-12 2xl:gap-20 w-full">
-                    <div className="flex flex-col gap-10 text-center xl:text-left xl:max-w-[33rem] 2xl:max-w-[40rem]">
-                        <motion.div initial={{ x: -40, opacity: 0 }} whileInView={{ x: 0, opacity: 1 }} transition={{ type: "spring", bounce: 0, duration: .8 }} viewport={{ once: true }} className={`flex flex-col gap-5`}>
-                            <h1 className={`${darkMode ? 'title-dark' : 'title-light'} text-3xl leading-[4rem] md:text-4xl md:leading-[4.5rem] lg:text-5xl lg:leading-[5rem] 2xl:text-6xl 2xl:leading-[5.5rem] font-bold break-words`}>Maßgeschneiderte Webentwicklung für Ihr Unternehmen</h1>
-                            <p className={`${darkMode ? 'description-dark font-light' : 'description-light'} 2xl:text-lg`}>Wir bieten maßgeschneiderte Lösungen, mit denen sich Ihr Unternehmen online von der Masse abhebt. Von Website-Design und -Entwicklung bis hin zur App-Programmierung und Suchmaschinenoptimierung kann unser Expertenteam Ihnen helfen, Ihre Online-Ziele zu erreichen.</p>
-                        </motion.div>
-                        <div className="flex justify-center xl:justify-start">
-							<button onClick={handleShowModal} className="btn-primary text-white uppercase w-fit py-2 sm:py-4 px-4 sm:px-8 font-medium bg-primary hover:bg-primary-2 transition-colors 2xl:text-lg rounded-sm">kostenloses Angebot erhalten</button>
-                            {/* <Link href={"/contact"}>
+        <>
+			<div className="relative overflow-hidden hidden sm:block" id="hero">
+				<Image className="absolute top-0 w-full h-full object-cover" src={`${darkMode ? '/home/hero/wave/dark/wave.webp' : '/home/hero/wave/light/wave.webp'}`} fill={true} alt="" priority />
+				<Navbar />
+				<section className="relative flex items-center justify-center px-6 sm:px-10 lg:px-20 2xl:px-0 min-h-[57rem] sm:min-h-[55rem] md:min-h-[65rem] lg:min-h-[75rem] xl:min-h-[50rem]" style={{ height: 'calc(100vh - 5rem)', zIndex: '1' }}>
+					<div className="max-w-7xl 2xl:max-w-[90rem] flex flex-col xl:flex-row items-center gap-12 2xl:gap-20 w-full">
+						<div className="flex flex-col gap-10 text-center xl:text-left xl:max-w-[33rem] 2xl:max-w-[40rem]">
+							<motion.div initial={{ x: -40, opacity: 0 }} whileInView={{ x: 0, opacity: 1 }} transition={{ type: "spring", bounce: 0, duration: .8 }} viewport={{ once: true }} className={`flex flex-col gap-5`}>
+								<h1 className={`${darkMode ? 'title-dark' : 'title-light'} text-3xl leading-[4rem] md:text-4xl md:leading-[4.5rem] lg:text-5xl lg:leading-[5rem] 2xl:text-6xl 2xl:leading-[5.5rem] font-bold break-words`}>Maßgeschneiderte Webentwicklung für Ihr Unternehmen</h1>
+								<p className={`${darkMode ? 'description-dark font-light' : 'description-light'} 2xl:text-lg`}>Wir bieten maßgeschneiderte Lösungen, mit denen sich Ihr Unternehmen online von der Masse abhebt. Von Website-Design und -Entwicklung bis hin zur App-Programmierung und Suchmaschinenoptimierung kann unser Expertenteam Ihnen helfen, Ihre Online-Ziele zu erreichen.</p>
+							</motion.div>
+							<div className="flex justify-center xl:justify-start">
+								<button onClick={handleShowModal} className="btn-primary text-white uppercase w-fit py-2 sm:py-4 px-4 sm:px-8 font-medium bg-primary hover:bg-primary-2 transition-colors 2xl:text-lg rounded-sm">kostenloses Angebot erhalten</button>
+								{/* <Link href={"/contact"}>
                             </Link> */}
-                        </div>
-                    </div>
-                    <motion.div initial={{ y: -60, opacity: 0 }} whileInView={{ y: 0, opacity: 1 }} transition={{ type: "spring", bounce: 0, duration: 1, delay: .2 }} viewport={{ once: true }} >
-                        <VideoPlayer 
-                            url={"https://res.cloudinary.com/drdor2wz7/video/upload/v1681591174/helphistech_eynapv.mp4"} 
-                        />
-                    </motion.div>
-                </div>
-            </section>
-			<AnimatePresence>
-				{showModal && (
-					<ContactModal blog={{ title: 'Home', url: "" }} handleClose={handleShowModal} language={'de'} />
-				)}
-			</AnimatePresence>
-        </div>
+							</div>
+						</div>
+						<motion.div initial={{ y: -60, opacity: 0 }} whileInView={{ y: 0, opacity: 1 }} transition={{ type: "spring", bounce: 0, duration: 1, delay: .2 }} viewport={{ once: true }} >
+							<VideoPlayer
+								url={"https://res.cloudinary.com/drdor2wz7/video/upload/v1681591174/helphistech_eynapv.mp4"}
+							/>
+						</motion.div>
+					</div>
+				</section>
+				<AnimatePresence>
+					{showModal && (
+						<ContactModal blog={{ title: 'Home', url: "" }} handleClose={handleShowModal} language={'de'} />
+					)}
+				</AnimatePresence>
+			</div>
+			<div className={"block sm:hidden"}>
+				<Navbar />
+				<div className={"hero-background min-h-[36rem]"}>
+					<div className={"flex flex-col gap-5 justify-end h-full p-8 z-10 relative"}>
+						<h1 className={"text-2xl xs:text-4xl text-center font-semibold"}>Maßgeschneiderte Webentwicklung für Ihr Unternehmen</h1>
+						<p className={"text-white text-center font-light"}>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto</p>
+						<div className={"grid grid-cols-2 gap-3 w-full"}>
+							<button className={"flex items-center gap-1 justify-center text-lg text-white rounded-full py-2 w-full border-2 border-white"}>
+								<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+									<path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+								</svg>
+								<span className={"font-light"}>Proceso</span>
+							</button>
+							<button className={"flex items-center gap-1 justify-center text-lg text-white rounded-full py-2 w-full border-2 border-white"}>
+								<span className={"font-light"}>Servicios</span>
+								<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+									<path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+								</svg>
+							</button>
+						</div>
+						<button className={"flex items-center gap-1 justify-center text-lg text-white rounded-full py-3 w-full bg-[#866bfef1]"}>
+							<span className={"font-light"}>Contactanos</span>
+							<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+								<path strokeLinecap="round" strokeLinejoin="round" d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3" />
+							</svg>
+						</button>
+						<div className={"flex items-center gap-2 gap-y-0 flex-wrap justify-center"}>
+							<p>¿Ya tienes un proyecto?</p>
+							<Link href={"/login"} className={"text-white underline"}>Inicia sesión</Link>
+						</div>						
+					</div>
+				</div>
+			</div>
+		</>
     )
 }
 
