fa438c0b56b405a6d06969e65f83d45f1ea1df7f
GelfisHorn <helphis.tech@gmail.com>
2023-08-02 16:47:06 -0300

    Update/Emails

--- client/src/pages/api/services/sendMail/helphistech.js
+++ client/src/pages/api/services/sendMail/helphistech.js
@@ -18,7 +18,7 @@ export default async function handler(req, res) {
 
         await transporter.sendMail({
             from: `"${name}" <${email}>`,
-            to: "mathiasbdev@gmail.com",
+            to: "helphis.tech@gmail.com",
             subject: `${name} está interesado/a en un servicio de HelphisTech`,
             text: `${name} está interesado/a en un servicio de HelphisTech\n
                 Servicio: ${service}\n
