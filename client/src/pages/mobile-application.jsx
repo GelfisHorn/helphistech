6b50e6aab7f83991237bb0a514d2e28d8cb9f3a6
GelfisHorn <helphis.tech@gmail.com>
2023-09-07 14:14:00 -0300

    Design/Benefits section on apps page

--- client/src/pages/mobile-application.jsx
+++ client/src/pages/mobile-application.jsx
@@ -1,4 +1,5 @@
 // Components
+import BenefitsSection from "@/components/BenefitsSection/Index";
 import ContactSection from "@/components/ContactSection";
 import Layout from "@/components/Layout";
 import AppsComments from "@/components/apps/Comments/Index";
@@ -20,6 +21,16 @@ export default function AppsPage() {
     return (
         <Layout title={"Entwicklung mobiler Anwendungen"} lang={"de"}>
             <AppsHero />
+            <BenefitsSection
+                title={"Steigere deine Online-Präsenz"}
+                subtitle={"Erfahre die Vorteile unseres Webentwicklungs-Services"}
+                content={[
+                    { icon: "fa-light fa-bullseye-pointer", title: "Schneller und Einfacher Zugang", description: "Deine Dienstleistungen sind nur einen Fingertipp entfernt, jederzeit und überall." },
+                    { icon: "fa-light fa-bell", title: "Persönliche Kommunikation", description: "Benachrichtigungen und exklusive Angebote, individuell auf die Bedürfnisse der Kunden zugeschnitten." },
+                    { icon: "fa-light fa-users", title: "Kundenbindung", description: "Aufbau von Kundenloyalität durch kundenzentrierte Erlebnisse." },
+                    { icon: "fa-light fa-lightbulb", title: "Spezifische Funktionen", description: "Nutze die einzigartigen Fähigkeiten von Mobilgeräten, um innovative Dienstleistungen anzubieten." }
+                ]}
+            />
             <AppsQuote />
             <AppsTechnologies />
             <AppsServices />
