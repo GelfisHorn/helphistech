a60ed722d8784710f0be08a3855a609b93e4bbca
GelfisHorn <helphis.tech@gmail.com>
2023-09-11 15:44:31 -0300

    Design/Bootcamp page (FAQ)

---
+++ client/src/mockData/bootcamp/faqs.js
@@ -0,0 +1,28 @@
+const es = [
+    {
+        title: "¿Hacen falta conocimientos previos?",
+        description: "No, el curso básico está diseñado para personas que estén empezando desde cero.",
+    },
+    {
+        title: "¿Se entrega algún diploma al finalizar?",
+        description: "Sí, al finalizar el curso se entrega un diploma acreditativo a todos los asistentes que hayan aprovechado el curso.",
+    },
+    {
+        title: "¿Podré realizar mi propio proyecto?",
+        description: "Sí, al final de cada mes se te asignará un proyecto emocionante, el cual podrás incluir luego en tu portafolio.",
+    },
+    {
+        title: "¿Existen tareas prácticas o proyectos a completar durante el curso?",
+        description: "Sí, todas las semanas recibirás ejercicios, los cuales te servirán para aplicar lo aprendido en las clases.",
+    },
+    {
+        title: "¿Las clases son pre grabadas o son impartidas por un profesor en línea?",
+        description: "Las clases son impartidas en línea por los mejores profesionales en el campo. Lo cual ofrece una comunicación directa con el profesor.",
+    }
+]
+
+const comments = {
+    es
+}
+
+export default comments;
