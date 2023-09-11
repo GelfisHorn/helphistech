e19779e310054a0e365416ff9ee482017f7a44f3
GelfisHorn <helphis.tech@gmail.com>
2023-09-11 15:44:02 -0300

    Design/Bootcamp page (Comments)

---
+++ client/src/mockData/bootcamp/comments.js
@@ -0,0 +1,44 @@
+const es = [
+    {
+        name: "Ana G.",
+        title: "Front-end",
+        description: "Aprender Front-end me abrió las puertas a un mundo de creatividad y oportunidades. Ahora puedo dar vida a diseños asombrosos y ver mi trabajo en línea. Cambió mi vida para mejor.",
+        gender: "female"
+    },
+    {
+        name: "Carlos M.",
+        title: "Back-end",
+        description: "El Back-end me proporcionó una comprensión profunda de cómo funcionan las aplicaciones web. Esto no solo me dio una carrera sólida, sino que también mejoró mi resolución de problemas en la vida diaria.",
+        gender: "male"
+    },
+    {
+        name: "Elena R.",
+        title: "Full-stack",
+        description: "Convertirme en un desarrollador Full-stack me dio la versatilidad que necesitaba en mi carrera. Ahora puedo trabajar en cualquier parte del desarrollo web y esto ha tenido un impacto positivo en mi vida laboral y personal.",
+        gender: "female"
+    },
+    {
+        name: "David S.",
+        title: "Front-end",
+        description: "El Front-end me permitió mezclar mi pasión por el diseño con la programación. Ahora tengo la capacidad de construir experiencias de usuario impresionantes y mi vida profesional está en auge.",
+        gender: "male"
+    },
+    {
+        name: "María L.",
+        title: "Back-end",
+        description: "Aprender Back-end me dio un conjunto único de habilidades. Puedo crear sistemas robustos y escalables, y esto me ha proporcionado estabilidad y satisfacción en mi carrera.",
+        gender: "female"
+    },
+    {
+        name: "Juan P.",
+        title: "Full-stack",
+        description: "Ser un desarrollador Full-stack me brindó la oportunidad de trabajar en proyectos desde cero hasta la implementación final. Esto ha mejorado mi creatividad y me ha permitido enfrentar desafíos con confianza.",
+        gender: "male"
+    }
+]
+
+const comments = {
+    es
+}
+
+export default comments;
